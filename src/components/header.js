4cb605e6ba4adb147f8e6920940f5964e3045801
farosanroman <ppazpurua@gmail.com>
2019-01-09 22:37:00 -0400

    ppa

--- src/components/header.js
+++ src/components/header.js
@@ -23,7 +23,7 @@ class Header extends Component {
         this.setState({
           isNavOpen: !this.state.isNavOpen
         });
-        alert('toggle')
+        //alert('toggle')
       }
   render() {
     let { flagLogin,flagMenu,flagMenu9D } = this.state;
